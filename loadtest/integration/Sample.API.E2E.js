d6751f55ed5c2b29ca766066fc90d64a00a01d95
Michael Costa <michaelbh@gmail.com>
2023-01-16 16:42:21 -0300

    Update Sample.API.E2E.js

--- loadtest/integration/Sample.API.E2E.js
+++ loadtest/integration/Sample.API.E2E.js
@@ -18,6 +18,37 @@ export default function () {
 };
 
 function funcao1(X) {
-    const res = http.get('##SUBSTITUA COM A URL DA SUA APLICACAO##');
+    const res = http.get('');
     return funcao2(result);
+}
+
+//Sample API end-to-end load test (fake)
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  duration: '20s',
+  vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<15000'],
+  },
+};
+
+export default function () {  
+  const res = http.get('##SUBSTITUA COM A URL DA SUA APLICACAO##');
+  console.info(res);
+  let res2 = funcaoA(3);
+  console.info(res2);
+  sleep(1);
+};
+
+function funcaoA(X) {
+    const res = http.get('##SUBSTITUA COM A URL DA SUA APLICACAO##');
+    return funcaoB(4);
+}
+
+function funcaoB(X) {
+    const res = http.get('##SUBSTITUA COM A URL DA SUA APLICACAO##');
+    return res;
 }
