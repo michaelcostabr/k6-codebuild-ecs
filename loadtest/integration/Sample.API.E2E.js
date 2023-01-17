3626b9b5a81a9f67123647f27072717b35c1c06e
Michael Costa <michaelbh@gmail.com>
2023-01-16 22:23:50 -0300

    Develop (#1)
    * arquivos iniciais
    
    * Update buildspec.yml
    
    * Update buildspec.yml
    
    * Update buildspec.yml
    
    * Update buildspec-loadtest-image.yml
    
    * Update Sample.API.E2E.js
    
    * Update buildspec-loadtest-ecs.yml
    
    * Update buildspec-loadtest-ecs.yml
    
    * Update buildspec-loadtest-ecs.yml
    
    * Update Sample.API.E2E.js
    
    * Delete taskdef-loadtest.json
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Update Sample.API.csproj

---
+++ loadtest/integration/Sample.API.E2E.js
@@ -0,0 +1,30 @@
+//Sample API end-to-end load test (fake)
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  duration: '20s',
+  vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<15000'],
+  },
+};
+
+export default function () {  
+  const res = http.get('##SUBSTITUA COM A URL DA SUA APLICACAO##');
+  //console.info(res);
+  let res2 = funcaoA(3);
+  //console.info(res2);
+  sleep(1);
+};
+
+function funcaoA(X) {
+    const res = http.get('##SUBSTITUA COM A URL DA SUA APLICACAO##');
+    return funcaoB(4);
+}
+
+function funcaoB(X) {
+    const res = http.get('##SUBSTITUA COM A URL DA SUA APLICACAO##');
+    return res;
+}
---
+++ src/Sample.API/Program.cs
@@ -0,0 +1,53 @@
+﻿try
+{
+    Console.WriteLine("Inicio");
+    var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+
+    var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: MyAllowSpecificOrigins,
+                          policy =>
+                          {
+                              policy.AllowAnyHeader();
+                              policy.AllowAnyOrigin();
+                              policy.AllowAnyMethod();
+                          });
+    });
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddHealthChecks();
+
+    var app = builder.Build();
+
+    app.MapHealthChecks("/health");
+
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //}
+
+    //app.UseHttpsRedirection();
+    app.UseCors();
+    app.UseAuthorization();
+
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+
+    app.MapControllers().RequireCors(MyAllowSpecificOrigins);
+
+
+    app.Run();
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
